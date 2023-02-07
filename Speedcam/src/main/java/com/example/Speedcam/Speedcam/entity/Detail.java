f6230f46529c45a71fc2b280a781a4444375c369
Aesys\Aesys <francesca.dicesare@aesys.tech>
2023-02-07 13:00:36 +0100

    commit entity

--- Speedcam/src/main/java/com/example/Speedcam/Speedcam/entity/Detail.java
+++ Speedcam/src/main/java/com/example/Speedcam/Speedcam/entity/Detail.java
@@ -1,7 +1,62 @@
 package com.example.Speedcam.Speedcam.entity;
 
-import jakarta.persistence.Entity;
+
+import javax.persistence.*;
 
 @Entity
 public class Detail {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "fiscal_code")
+    private String fiscalCode;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "address")
+    private String address;
+    @OneToOne(mappedBy = "registry", cascade = CascadeType.ALL)
+    private Registry registry;
+
+    public Detail() {
+    }
+
+    public Detail(Integer id, String fiscalCode, String phoneNumber, String address) {
+        this.id = id;
+        this.fiscalCode = fiscalCode;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFiscalCode() {
+        return fiscalCode;
+    }
+
+    public void setFiscalCode(String fiscalCode) {
+        this.fiscalCode = fiscalCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
--- Speedcam/src/main/java/com/example/Speedcam/Speedcam/entity/Registry.java
+++ Speedcam/src/main/java/com/example/Speedcam/Speedcam/entity/Registry.java
@@ -1,7 +1,57 @@
 package com.example.Speedcam.Speedcam.entity;
 
-import jakarta.persistence.Entity;
+
+import javax.persistence.*;
 
 @Entity
 public class Registry {
+    @Id
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_detail")
+    private Detail detail;
+
+    public Registry(Integer id, String name, String surname, Detail detail) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.detail = detail;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Detail getDetail() {
+        return detail;
+    }
+
+    public void setDetail(Detail detail) {
+        this.detail = detail;
+    }
 }
