7a8dcf8a319b1c583fe3ded342d6aab00f0b51a6
gabrieledirocco <117287427+gabrieledirocco@users.noreply.github.com>
2023-02-07 12:16:10 +0100

    Add files via upload
    upload project

---
+++ Speedcam/src/main/java/com/example/Speedcam/Speedcam/SpeedcamApplication.java
@@ -0,0 +1,13 @@
+package com.example.Speedcam.Speedcam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedcamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedcamApplication.class, args);
+	}
+
+}
---
+++ Speedcam/src/test/java/com/example/Speedcam/Speedcam/SpeedcamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Speedcam.Speedcam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedcamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
