b999abbee7e82df846d529f3a732a0e7a6bf16bb
GiacomoVervenna <giacomo.vervenna@aesys.tech>
2023-02-08 17:48:42 +0100

    test create fine method

---
+++ Speedcam/src/test/java/com/example/Speedcam/Speedcam/services_tests/TestFineService.java
@@ -0,0 +1,45 @@
+package com.example.Speedcam.Speedcam.services_tests;
+
+import com.example.Speedcam.Speedcam.entity.Fine;
+import com.example.Speedcam.Speedcam.entity.mapperDTO.MapperDTO;
+import com.example.Speedcam.Speedcam.repository.FineRepository;
+import com.example.Speedcam.Speedcam.service.impl.FineServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestFineService {
+
+    @InjectMocks
+    private FineServiceImpl service;
+    @Mock
+    private FineRepository repo;
+
+    @Mock
+    private MapperDTO mapper;
+
+    @Test
+    void testCreateFineMethod() {
+
+        Fine fine = new Fine();
+        Assertions.assertDoesNotThrow(() -> {
+            service.create(fine);
+        });
+    }
+
+    @Test
+    void testCreateFineMethod_NullPointerException() {
+
+        Fine fine = new Fine();
+        Mockito.when(repo.save(fine)).thenThrow(new NullPointerException());
+
+        Assertions.assertThrows(NullPointerException.class, () ->
+                service.create(fine));
+    }
+}
+
+
